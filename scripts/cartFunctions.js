026a473dd5e12cb3e3227b937ffc2d3b07da605b
HerculesMatheus <hercules.silva@qualitydigital.global>
2025-09-29 16:45:35 -0300

    feat: switch Vanilla JS to jQuery

--- scripts/cartFunctions.js
+++ scripts/cartFunctions.js
@@ -2,178 +2,177 @@ var cartList = JSON.parse(localStorage.getItem("list")) || [];
 var checkoutList = [];
 
 function drawCartItems(item) {
-  const container = document.querySelector(".cart__itemsContainer");
+  const $container = $(".cart__itemsContainer");
 
-  const cartItem = document.createElement("div");
-  cartItem.classList.add("cart__item");
-  cartItem.setAttribute("id", item.id);
+  const $cartItem = $("<div></div>").addClass("cart__item").attr("id", item.id);
 
-  const itemImg = document.createElement("img");
-  itemImg.src = item.image;
-  itemImg.alt = item.name;
-  itemImg.classList.add("item__img");
+  const $itemImg = $("<img>")
+    .attr("src", item.image)
+    .attr("alt", item.name)
+    .addClass("item__img");
 
-  const itemInfo = document.createElement("div");
-  itemInfo.classList.add("item__info");
+  const $itemInfo = $("<div></div>").addClass("item__info");
 
-  const itemInfoTitle = document.createElement("p");
-  itemInfoTitle.innerText = item.name;
-  itemInfoTitle.classList.add("item__title");
+  const $itemInfoTitle = $("<p></p>").text(item.name).addClass("item__title");
 
-  const itemInfoQuantity = document.createElement("div");
-  itemInfoQuantity.classList.add("item__quantity");
+  const $itemInfoQuantity = $("<div></div>").addClass("item__quantity");
 
-  const itemInfoQuantityValue = document.createElement("p");
-  itemInfoQuantityValue.classList.add("item__quantity--value");
-  itemInfoQuantityValue.innerText = item.quantity.toString();
+  const $itemInfoQuantityValue = $("<p></p>")
+    .addClass("item__quantity--value")
+    .text(item.quantity.toString());
 
-  const itemInfoQuantityMinus = document.createElement("button");
-  itemInfoQuantityMinus.classList.add("item__quantity--minus");
-  itemInfoQuantityMinus.dataset.id = item.id;
-  itemInfoQuantityMinus.addEventListener("click", () => {
-    const storagedItem = cartList.find(
-      (storagedItem) => storagedItem.id == item.id
-    );
-    if (storagedItem.quantity > 1) {
-      storagedItem.quantity -= 1;
-      itemInfoQuantityValue.innerText = storagedItem.quantity.toString();
-    }
-    checkoutList = cartList.filter(
-      (storagedItem) => storagedItem.selected === true
-    );
-    localStorage.setItem("checkoutList", JSON.stringify(checkoutList));
-    localStorage.setItem("list", JSON.stringify(cartList));
-    showPriceAndQuantity(checkoutList);
-  });
-
-  const itemInfoQuantityMinusImg = document.createElement("img");
-  itemInfoQuantityMinusImg.src = "./assets/minus.svg";
-  itemInfoQuantityMinusImg.alt = "Reduzir quantidade";
-
-  const itemInfoQuantityPlus = document.createElement("button");
-  itemInfoQuantityPlus.classList.add("item__quantity--plus");
-  itemInfoQuantityPlus.dataset.id = item.id;
-  itemInfoQuantityPlus.addEventListener("click", () => {
-    const storagedItem = cartList.find(
-      (storagedItem) => storagedItem.id == item.id
-    );
-    if (storagedItem.quantity < 99) {
-      storagedItem.quantity += 1;
-      itemInfoQuantityValue.innerText = storagedItem.quantity.toString();
-    }
-    checkoutList = cartList.filter(
-      (storagedItem) => storagedItem.selected === true
-    );
-    localStorage.setItem("checkoutList", JSON.stringify(checkoutList));
-    localStorage.setItem("list", JSON.stringify(cartList));
-    showPriceAndQuantity(checkoutList);
-  });
-
-  const itemInfoQuantityPlusImg = document.createElement("img");
-  itemInfoQuantityPlusImg.src = "./assets/plus.svg";
-  itemInfoQuantityPlusImg.alt = "Aumentar quantidade";
-
-  const itemPrice = document.createElement("p");
-  itemPrice.innerText = `R$ ${item.price}`;
-  itemPrice.classList.add("item__price");
-  itemPrice.setAttribute("value", item.price);
-
-  const deleteButton = document.createElement("button");
-  deleteButton.classList.add("item__buttons--delete");
-  deleteButton.dataset.id = item.id;
-  deleteButton.addEventListener("click", () => {
-    cartItem.remove();
-    cartList = cartList.filter((storagedItem) => storagedItem.id !== item.id);
-    localStorage.setItem("list", JSON.stringify(cartList));
-    checkoutList = checkoutList.filter(
-      (storagedItem) => storagedItem.id !== item.id
-    );
-    localStorage.setItem("checkoutList", JSON.stringify(checkoutList));
-    showPriceAndQuantity(checkoutList);
-  });
-
-  const deleteButtonImg = document.createElement("img");
-  deleteButtonImg.src = "./assets/delete.svg";
-  deleteButtonImg.alt = "Remover do carrinho";
-
-  const checkButton = document.createElement("button");
-  checkButton.classList.add("item__buttons--select");
-  checkButton.dataset.checked = "true";
-  checkButton.addEventListener("click", () => {
-    const storagedItem = cartList.find(
-      (storagedItem) => storagedItem.id === item.id
-    );
-    if (checkButton.dataset.checked === "false") {
-      checkButtonImg.src = "./assets/checked.svg";
-      checkButton.dataset.checked = "true";
-      storagedItem.selected = true;
-      localStorage.setItem("list", JSON.stringify(cartList));
+  const $itemInfoQuantityMinus = $("<button>")
+    .addClass("item__quantity--minus")
+    .attr("data-id", item.id)
+    .on("click", function () {
+      const storagedItem = cartList.find(
+        (storagedItem) => storagedItem.id == item.id
+      );
+      if (storagedItem.quantity > 1) {
+        storagedItem.quantity -= 1;
+        $itemInfoQuantityValue.text(storagedItem.quantity.toString());
+      }
       checkoutList = cartList.filter(
-        (storagedItem) => storagedItem.selected == true
+        (storagedItem) => storagedItem.selected === true
+      );
+      localStorage.setItem("checkoutList", JSON.stringify(checkoutList));
+      localStorage.setItem("list", JSON.stringify(cartList));
+      showPriceAndQuantity(checkoutList);
+    });
+
+  const $itemInfoQuantityMinusImg = $("<img>")
+    .attr("src", "./assets/minus.svg")
+    .attr("alt", "Reduzir quantidade");
+
+  const $itemInfoQuantityPlus = $("<button>")
+    .addClass("item__quantity--plus")
+    .attr("data-id", item.id)
+    .on("click", function () {
+      const storagedItem = cartList.find(
+        (storagedItem) => storagedItem.id == item.id
+      );
+      if (storagedItem.quantity < 99) {
+        storagedItem.quantity += 1;
+        $itemInfoQuantityValue.text(storagedItem.quantity.toString());
+      }
+      checkoutList = cartList.filter(
+        (storagedItem) => storagedItem.selected === true
+      );
+      localStorage.setItem("checkoutList", JSON.stringify(checkoutList));
+      localStorage.setItem("list", JSON.stringify(cartList));
+      showPriceAndQuantity(checkoutList);
+    });
+
+  const $itemInfoQuantityPlusImg = $("<img>")
+    .attr("src", "./assets/plus.svg")
+    .attr("alt", "Aumentar quantidade");
+
+  const $itemPrice = $("<p>")
+    .text(`R$ ${item.price}`)
+    .addClass("item__price")
+    .attr("value", item.price);
+
+  const $deleteButton = $("<button>")
+    .addClass("item__buttons--delete")
+    .attr("data-id", item.id)
+    .on("click", function () {
+      $cartItem.remove();
+      cartList = cartList.filter((storagedItem) => storagedItem.id !== item.id);
+      localStorage.setItem("list", JSON.stringify(cartList));
+      checkoutList = checkoutList.filter(
+        (storagedItem) => storagedItem.id !== item.id
       );
       localStorage.setItem("checkoutList", JSON.stringify(checkoutList));
       showPriceAndQuantity(checkoutList);
-      return;
-    }
-    checkButtonImg.src = "./assets/unchecked.svg";
-    checkButton.dataset.checked = "false";
-    storagedItem.selected = false;
-    localStorage.setItem("list", JSON.stringify(cartList));
-    checkoutList = cartList.filter(
-      (storagedItem) => storagedItem.selected == true
-    );
-    localStorage.setItem("checkoutList", JSON.stringify(checkoutList));
-    showPriceAndQuantity(checkoutList);
-  });
+    });
 
-  const checkButtonImg = document.createElement("img");
-  checkButtonImg.classList.add("button--select--img");
-  checkButtonImg.src = "./assets/checked.svg";
-  checkButtonImg.alt = "Selecionar produto";
+  const $deleteButtonImg = $("<img>")
+    .attr("src", "./assets/delete.svg")
+    .attr("alt", "Remover do carrinho");
 
-  itemInfoQuantityMinus.appendChild(itemInfoQuantityMinusImg);
-  itemInfoQuantityPlus.appendChild(itemInfoQuantityPlusImg);
+  const $checkButtonImg = $("<img>")
+    .addClass("button--select--img")
+    .attr("src", "./assets/checked.svg")
+    .attr("alt", "Selecionar produto");
 
-  itemInfoQuantity.appendChild(itemInfoQuantityMinus);
-  itemInfoQuantity.appendChild(itemInfoQuantityValue);
-  itemInfoQuantity.appendChild(itemInfoQuantityPlus);
+  const $checkButton = $("<button>")
+    .addClass("item__buttons--select")
+    .attr("data-checked", "true")
+    .on("click", function () {
+      const storagedItem = cartList.find(
+        (storagedItem) => storagedItem.id === item.id
+      );
 
-  itemInfo.appendChild(itemInfoTitle);
-  itemInfo.appendChild(itemInfoQuantity);
+      const isChecked = $(this).attr("data-checked") === "true";
 
-  deleteButton.appendChild(deleteButtonImg);
-  checkButton.appendChild(checkButtonImg);
+      if (!isChecked) {
+        $checkButtonImg.attr("src", "./assets/checked.svg");
+        $(this).attr("data-checked", "true");
+        storagedItem.selected = true;
+      } else {
+        $checkButtonImg.attr("src", "./assets/unchecked.svg");
+        $(this).attr("data-checked", "false");
+        storagedItem.selected = false;
+      }
 
-  cartItem.appendChild(checkButton);
-  cartItem.appendChild(itemImg);
-  cartItem.appendChild(itemInfo);
-  cartItem.appendChild(itemPrice);
-  cartItem.appendChild(deleteButton);
+      localStorage.setItem("list", JSON.stringify(cartList));
+      checkoutList = cartList.filter(
+        (storagedItem) => storagedItem.selected === true
+      );
+      localStorage.setItem("checkoutList", JSON.stringify(checkoutList));
+      showPriceAndQuantity(checkoutList);
+    });
 
-  container.prepend(cartItem);
+  $checkButton.append($checkButtonImg);
+
+  $itemInfoQuantityMinus.append($itemInfoQuantityMinusImg);
+  $itemInfoQuantityPlus.append($itemInfoQuantityPlusImg);
+
+  $itemInfoQuantity.append($itemInfoQuantityMinus);
+  $itemInfoQuantity.append($itemInfoQuantityValue);
+  $itemInfoQuantity.append($itemInfoQuantityPlus);
+
+  $itemInfo.append($itemInfoTitle);
+  $itemInfo.append($itemInfoQuantity);
+
+  $deleteButton.append($deleteButtonImg);
+  $checkButton.append($checkButtonImg);
+
+  $cartItem.append($checkButton);
+  $cartItem.append($itemImg);
+  $cartItem.append($itemInfo);
+  $cartItem.append($itemPrice);
+  $cartItem.append($deleteButton);
+
+  $cartItem.hide().prependTo($container).slideDown("normal");
   showPriceAndQuantity(checkoutList);
 }
 
 function selectAllItems() {
-  const selectAll = document.querySelector(".item__buttons--select-all");
-  const selectAllImg = document.querySelector(".select-all--image");
-  selectAll.addEventListener("click", () => {
-    const checked = selectAll.dataset.checked === "false";
+  const $selectAll = $(".item__buttons--select-all");
+  const $selectAllImg = $(".select-all--image");
 
-    selectAllImg.src = checked
-      ? "./assets/checked.svg"
-      : "./assets/unchecked.svg";
-    selectAll.dataset.checked = checked ? "true" : "false";
+  $selectAll.on("click", function () {
+    const checked = $(this).attr("data-checked") === "false";
 
-    const selectOnly = document.querySelectorAll(".item__buttons--select");
-    const selectOnlyImg = document.querySelectorAll(".button--select--img");
+    $selectAllImg.attr(
+      "src",
+      checked ? "./assets/checked.svg" : "./assets/unchecked.svg"
+    );
+    $(this).attr("data-checked", checked ? "true" : "false");
 
-    selectOnly.forEach((btn, index) => {
-      btn.dataset.checked = checked ? "true" : "false";
-      selectOnlyImg[index].src = checked
-        ? "./assets/checked.svg"
-        : "./assets/unchecked.svg";
-      cartList[index].selected = checked; // atualiza cada storagedItem
+    const $selectOnly = $(".item__buttons--select");
+    const $selectOnlyImg = $(".button--select--img");
+
+    $selectOnly.each(function (index) {
+      $(this).attr("data-checked", checked ? "true" : "false");
+      $selectOnlyImg
+        .eq(index)
+        .attr(
+          "src",
+          checked ? "./assets/checked.svg" : "./assets/unchecked.svg"
+        );
+      cartList[index].selected = checked;
     });
 
     checkoutList = checked ? [...cartList] : [];
@@ -220,14 +219,11 @@ function calculateTotalPrice(item) {
 }
 
 function showPriceAndQuantity(item) {
-  const discountDisplay = document.querySelector(".discount__value");
-  const priceDisplay = document.querySelector(".total__value");
-  const quantityDisplay = document.querySelector(".showCart__quantity");
   const { totalPrice, discount } = calculateTotalPrice(item);
 
-  discountDisplay.innerText = `R$ ${discount.toString()}`;
-  priceDisplay.innerText = `R$ ${totalPrice.toString()}`;
-  quantityDisplay.innerText = calculateQuantity();
+  $(".discount__value").text(`R$ ${discount.toString()}`);
+  $(".total__value").text(`R$ ${totalPrice.toString()}`);
+  $(".showCart__quantity").text(calculateQuantity());
 }
 
 function calculateQuantity() {
--- scripts/drawGridCards.js
+++ scripts/drawGridCards.js
@@ -1,40 +1,32 @@
 import { addToCart } from "./cartFunctions.js";
 
 function drawGridCards(item) {
-  const section = document.querySelector(".productSession__grid");
+  const $section = $(".productSession__grid");
 
-  const card = document.createElement("div");
-  card.classList.add("grid__card");
-  card.setAttribute("id", item.id);
+  const $card = $("<div>").addClass("grid__card").attr("id", item.id);
 
-  const img = document.createElement("img");
-  img.src = item.image;
-  img.alt = item.name;
-  img.classList.add("card__image");
+  const $img = $("<img>")
+    .attr("src", item.image)
+    .attr("alt", item.name)
+    .addClass("card__image");
 
-  const h3 = document.createElement("h3");
-  h3.innerText = item.name;
-  h3.classList.add("card__title");
+  const $h3 = $("<h3>").text(item.name).addClass("card__title");
 
-  const p = document.createElement("p");
-  p.innerText = `R$ ${item.price}`;
-  p.classList.add("card__price");
-  p.setAttribute("value", item.price);
-  p.setAttribute("discount", item.discounts ? item.discounts : "0");
+  const $p = $("<p>")
+    .text(`R$ ${item.price}`)
+    .addClass("card__price")
+    .attr("value", item.price)
+    .attr("discount", item.discounts ? item.discounts : "0");
 
-  const button = document.createElement("button");
-  button.innerText = "Adicionar ao Carrinho";
-  button.classList.add("card__button");
-  button.addEventListener("click", () => {
-    addToCart(item);
-  });
+  const $button = $("<button>")
+    .text("Adicionar ao Carrinho")
+    .addClass("card__button")
+    .on("click", () => {
+      addToCart(item);
+    });
 
-  card.appendChild(img);
-  card.appendChild(button);
-  card.appendChild(h3);
-  card.appendChild(p);
-
-  section.appendChild(card);
+  $card.append($img, $button, $h3, $p);
+  $card.hide().prependTo($section).fadeIn(400);
 }
 
 export default drawGridCards;
